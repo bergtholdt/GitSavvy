97e80d587babac2dfc86931c040836326d5ccb3a
Martin Bergtholdt <martin.bergtholdt@philips.com>
2016-07-11 15:13:24 +0200

    Enhancement: open gs_show_commit in other view if open_views_in_other_group is true

--- core/commands/show_commit.py
+++ core/commands/show_commit.py
@@ -1,6 +1,7 @@
 from sublime_plugin import WindowCommand, TextCommand
 
 from ..git_command import GitCommand
+from ...common import util
 
 
 SHOW_COMMIT_TITLE = "COMMIT: {}"
@@ -10,15 +11,13 @@ class GsShowCommitCommand(WindowCommand, GitCommand):
 
     def run(self, commit_hash):
         repo_path = self.repo_path
-        view = self.window.new_file()
+        view = util.view.get_scratch_view(self, 'show_commit')
         view.set_syntax_file("Packages/GitSavvy/syntax/show_commit.sublime-syntax")
-        view.settings().set("git_savvy.show_commit_view", True)
         view.settings().set("git_savvy.show_commit_view.commit", commit_hash)
         view.settings().set("git_savvy.repo_path", repo_path)
         view.settings().set("word_wrap", False)
         view.settings().set("line_numbers", False)
         view.set_name(SHOW_COMMIT_TITLE.format(commit_hash[:7]))
-        view.set_scratch(True)
         view.run_command("gs_show_commit_initialize_view")
 
 
